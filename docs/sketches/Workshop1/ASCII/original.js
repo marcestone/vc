571fca867acbf78ef75f4becf928978ae12414e0
Juan Gomez <jgomez@workapps.com.co>
2021-04-06 08:55:11 -0500

    Fixes

--- docs/sketches/Workshop1/ASCII/original.js
+++ docs/sketches/Workshop1/ASCII/original.js
@@ -1,10 +1,9 @@
 function preload() {
-    img = loadImage('/vc/docs/sketches/Workshop1/ASCII/example7.jpg');
+    img = loadImage('/vc/docs/sketches/Workshop1/ASCII/example8.jpg');
   }
   
   function setup() {
-    createCanvas(640, 480); 
-    textAlign(CENTER, CENTER); textFont('monospace', 8); textStyle(NORMAL);
+    createCanvas(400, 480); 
     noStroke(); fill(255);
     frameRate(30);
   }
